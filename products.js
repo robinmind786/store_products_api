749aa5eb19da93d9df3f6aa30dbc2fbca1a458fc
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2021-08-30 14:57:59 +0600

    Add files via upload

---
+++ products.js
@@ -0,0 +1,470 @@
+[
+    {
+        "id": "01",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "featured": true,
+            "price": 2224,
+            "title": "Gatorade Whey Protein Recover Bars, Peanut Butter Chocolate",
+            "image": [
+                {
+                    "id": "01",
+                    "url": "./img/products/01.jpg",
+                    "filename": "product-1.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/01.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/01.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/01.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "02",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "featured": true,
+            "price": 1390,
+            "title": "Soft Croissant, Caramel Dulce De Leche Filling, Perfect Breakfast",
+            "image": [
+                {
+                    "id": "02",
+                    "url": "./img/products/02.jpg",
+                    "filename": "product-2.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/02.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/02.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/02.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "03",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "featured": true,
+            "price": 1299,
+            "title": "Grandma's Cookies Variety Pack of 30",
+            "image": [
+                {
+                    "id": "03",
+                    "url": "./img/products/03.jpg",
+                    "filename": "product-3.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/03.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/03.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/03.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "04",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "featured": true,
+            "price": 1999,
+            "title": "Milk-Bone Flavor Snacks Dog Treats for Dogs",
+            "image": [
+                {
+                    "id": "04",
+                    "url": "./img/products/04.jpg",
+                    "filename": "product-4.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/04.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/04.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/04.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "05",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "featured": true,
+            "price": 2000,
+            "title": "Sambucus Sugar-Free Elderberry Syrup, Herbal Supplements",
+            "image": [
+                {
+                    "id": "05",
+                    "url": "./img/products/05.jpg",
+                    "filename": "product-5.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/05.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/05.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/05.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "06",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "featured": true,
+            "price": 1399,
+            "title": "3 Pack Organic and Unsweetned Barbeque & Steak Sauce",
+            "image": [
+                {
+                    "id": "06",
+                    "url": "./img/products/06.jpg",
+                    "filename": "product-6.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/06.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/06.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/06.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "07",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "newpro": true,
+            "price": 2224,
+            "title": "Gatorade Whey Protein Recover Bars, Peanut Butter Chocolate",
+            "image": [
+                {
+                    "id": "07",
+                    "url": "./img/products/07.jpg",
+                    "filename": "product-1.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/07.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/07.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/01.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "08",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "newpro": true,
+            "price": 98,
+            "title": "Whiskas Adult Cat Mackerel and Sardines Mackerel and sardine",
+            "image": [
+                {
+                    "id": "08",
+                    "url": "./img/products/08.jpg",
+                    "filename": "product-8.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/08.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/08.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/02.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "09",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "newpro": true,
+            "price": 148,
+            "title": "BUTTERNUT SQUASH",
+            "image": [
+                {
+                    "id": "09",
+                    "url": "./img/products/09.jpg",
+                    "filename": "product-9.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/09.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/09.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/03.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "10",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "newpro": true,
+            "price": 349,
+            "title": "LETTUCE – SPRING MIX",
+            "image": [
+                {
+                    "id": "10",
+                    "url": "./img/products/10.jpg",
+                    "filename": "product-10.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/10.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/10.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/04.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "11",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "newpro": true,
+            "price": 2154,
+            "title": "CAULIFLOWER 500G",
+            "image": [
+                {
+                    "id": "11",
+                    "url": "./img/products/11.jpg",
+                    "filename": "product-11.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/11.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/11.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/05.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "id": "12",
+        "fields": {
+            "company": "ikea",
+            "colors": [
+                "#f15025",
+                "#222"
+            ],
+            "newpro": true,
+            "price": 845,
+            "title": "034 AVOCADO 500G",
+            "image": [
+                {
+                    "id": "12",
+                    "url": "./img/products/12.jpg",
+                    "filename": "product-12.jpg",
+                    "size": 62864,
+                    "type": "image/jpg",
+                    "thumbnails": {
+                        "small": {
+                            "url": "./img/products/12.jpg",
+                            "width": 56,
+                            "height": 36
+                        },
+                        "large1": {
+                            "url": "./img/products/12.jpg",
+                            "width": 149,
+                            "height": 149
+                        },
+                        "large2": {
+                            "url": "./img/products/overlay/06.jpg",
+                            "width": 149,
+                            "height": 149
+                        }
+                    }
+                }
+            ]
+        }
+    }
+]
